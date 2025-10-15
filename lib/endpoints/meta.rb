98468ebf3a73234ddae77fdbdde5532ebe948162
Ryan <ryan@iam-compliant.com>
2025-10-15 23:52:38 +0100

    Not in scope for this PR

--- lib/endpoints/meta.rb
+++ lib/endpoints/meta.rb
@@ -7,15 +7,5 @@ module Wonde
       self.uri = id + '/' + @@uri if id
       self.uri = self.uri.gsub("//", "/")
     end
-
-    def permissions(id, includes = Array.new(), parameters = Array.new())
-      self.uri = self.uri + '/permissions'
-      return self.get(id, includes, parameters)
-    end
-
-    def acl(id, includes = Array.new(), parameters = Array.new())
-      self.uri = self.uri + '/acl'
-      return self.get(id, includes, parameters)
-    end
   end
 end
